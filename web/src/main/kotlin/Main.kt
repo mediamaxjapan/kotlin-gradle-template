cd9b9c5347deeb14c446d2a128954c2108b3d3be
ONDA, Takashi <onda@mmj.ne.jp>
2019-06-10 08:25:38 +0700

    introduce simple main function.

--- web/src/main/kotlin/Main.kt
+++ web/src/main/kotlin/Main.kt
@@ -2,17 +2,14 @@
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import controllers.HelloController
-import org.jooby.Kooby
 import org.jooby.apitool.ApiTool
 import org.jooby.json.Jackson
 import org.jooby.run
 
-class App : Kooby({
-  use(Jackson(ObjectMapper()))
-  use(ApiTool().swagger("/swagger").raml("/raml"))
-  use(HelloController::class)
-})
-
 fun main(vararg args: String) {
-  run(::App, *args)
+  run(*args) {
+    use(Jackson(ObjectMapper()))
+    use(ApiTool().swagger("/swagger").raml("/raml"))
+    use(HelloController::class)
+  }
 }
